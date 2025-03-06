08a2c8265b119eb2300fb97031d4e89b127c7dc3
AmanGitkumar <kumaraman55475@gmail.com>
2025-03-06 12:41:08 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,56 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const cors = require('cors');
-// const bodyParser = require("body-parser");
-
-// // ✅ Import Routes
-// const AuthRouter = require('./Routes/AuthRouter');
-// const ExpenseRouter = require('./Routes/ExpenseRouter');
-// const CategoryRouter = require("./Routes/CategoryRouter");
-// const IncomeRouter = require("./Routes/IncomeRouter");
-// const DashboardRouter = require("./Routes/DashboardRouter");
-// const transactionRoutes = require("./Routes/TransactionRouter");
-// const billRoutes = require("./Routes/billRoutes");
-// const goalRoutes = require("./Routes/goalRouter"); // ✅ Goals Route (Authentication Removed)
-// const ensureAuthenticated = require('./Middlewares/Auth');
-// const notificationRoutes = require("./Routes/notificationRoutes"); // ✅ Import notification routes
-
-
-// require("./cronJobs/dueBillNotifier"); // ✅ Load Due Bill Notification Cron Job
-// require('dotenv').config();
-// require('./Models/db');
-// app.use(express.json()); // ✅ Allows parsing JSON request body
-// const PORT = process.env.PORT || 8080;
-
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cors({ origin: "https://f1-euwb.onrender.com", credentials: true })); // ✅ Allow frontend requests
-// app.use(bodyParser.json());
-
-// app.get('/ping', (req, res) => {
-//     res.send('PONG');
-// });
-
-
-
-
-
-// // ✅ Routes (Authorization Applied Where Needed)
-// app.use('/auth', AuthRouter);
-// app.use('/expenses', ensureAuthenticated, ExpenseRouter);
-// app.use('/categories', CategoryRouter);
-// app.use('/incomes', ensureAuthenticated, IncomeRouter);
-// app.use("/dashboard", DashboardRouter);
-// app.use("/transactions", transactionRoutes);
-// app.use("/api/bills", billRoutes);
-// app.use("/api/notifications", notificationRoutes); // ✅ Add notification routes
-
-// // ✅ 🚀 Goals Route (Authentication Removed)
-// app.use("/goals", goalRoutes);
-
-// app.listen(PORT, () => {
-//     console.log(`✅ Server is running on ${PORT}`);
-// });
 
 
 
@@ -84,7 +31,7 @@ app.use(bodyParser.json());
 
 // ✅ CORS Setup
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://finance-ps2-tracker.onrender.com'],
     credentials: true,
 }));
 
